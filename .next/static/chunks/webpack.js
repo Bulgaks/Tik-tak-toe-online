2cb9e3c811f0fe641cbfdf327e9da1f3b3fe4992
Bulgaks <bulgaks92@gmail.com>
2024-07-24 23:20:42 +0300

    Добавляет догику отрисовки символов

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e7e28731e64dea3"; }
+/******/ 		__webpack_require__.h = function() { return "8807de28d04a52d1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

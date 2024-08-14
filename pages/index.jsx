b112c456f6d6aec7c0f473c57b9cdb373958b5f8
Bulgaks <bulgaks92@gmail.com>
2024-08-14 14:51:07 +0300

    Добавляет модальное окно победы

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,12 +5,21 @@ import { GameTitle } from "../components/game/game-title.jsx";
 import Header from "../components/header/header.jsx";
 import { useGameState } from "../components/game/use-game-state.js";
 import { GameSymbol } from "../components/game/game-symbol.jsx";
+import { UiModal } from "../components/uikit/ui-modal.jsx";
+import { UiButton } from "../components/uikit/ui-button.jsx";
 
 export default function HomePage() {
   const [playersCount] = useState(4);
 
-  const { cells, currentMove, handleCellClick, nextMove, winnerSequence, handlePlayerTimeOver, winnerSymbol } =
-    useGameState(playersCount);
+  const {
+    cells,
+    currentMove,
+    handleCellClick,
+    nextMove,
+    winnerSequence,
+    handlePlayerTimeOver,
+    winnerSymbol,
+  } = useGameState(playersCount);
 
   return (
     <div className="bg-slate-50 min-h-screen">
@@ -25,8 +34,30 @@ export default function HomePage() {
           onPlayerTimeOver={handlePlayerTimeOver}
         />
         {winnerSymbol && (
-          <div><GameSymbol classname="my-4" symbol={winnerSymbol} /></div>
+          <div>
+            <GameSymbol classname="my-4" symbol={winnerSymbol} />
+          </div>
         )}
+        <UiModal
+          width="md"
+          isOpen={winnerSymbol}
+          onClose={() => console.log("close")}
+        >
+          <UiModal.Header>Игра завершена</UiModal.Header>
+          <UiModal.Body>
+            <div className="text-sm">
+              Победитель: <span className="text-teal-600">Bulgakov</span>
+            </div>
+          </UiModal.Body>
+          <UiModal.Footer>
+            <UiButton size="md" variant="outline">
+              Вернуться
+            </UiButton>
+            <UiButton size="md" variant="primary">
+              Играть снова
+            </UiButton>
+          </UiModal.Footer>
+        </UiModal>
         <GameField
           className="mt-6"
           cells={cells}
